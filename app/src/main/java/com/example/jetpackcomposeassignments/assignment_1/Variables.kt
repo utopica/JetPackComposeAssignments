47e7e5553ffe9c5c1d186b95976b35e83756dc85
Elif OKUMUŞ <98635931+utopica@users.noreply.github.com>
2024-08-08 10:04:29 +0300

    Created the first assignment

---
+++ app/src/main/java/com/example/jetpackcomposeassignments/assignment_1/Variables.kt
@@ -0,0 +1,51 @@
+fun main() {
+
+    val city = "Istanbul"
+    val country = "Turkey"
+    val phone = "+901111111111"
+    val postalCode = "34000"
+    val email = "jetpackcompose@jetpack.com"
+    val profession = "Software Engineer"
+    val stockQuantity = 1500
+    val customerName = "Jane Doe"
+    val balance = 123400.56
+    val birthDate = "20001-01-01"
+    val salary = 75000.0
+    val maritalStatus = "Single"
+    val productReview = "Quality product! Don't hesitate to buy it."
+    val paymentDate = "2024-08-08"
+    val payment = 5000.0
+    val orderQuantity = 3
+    val carModel = "Nissan Qashqai"
+    val bookTitle = "Programming with Kotlin for Dummies"
+    val publicationDate = "2023-08-08"
+    val discountAmount = 150.0
+    val roomCount = 2
+    val latitude = 41.0082
+    val longitude = 28.9784
+    val productName = "Laptop"
+    val mealPrice = 450.0
+    val brand = "Apple"
+    val musicTitle = "Blue Bird"
+    val videoDuration = 240
+    val productRating = 4.5
+    val imageName = "girl_with_a_pearl_earring.jpg"
+    val fileType = "jpg"
+    val color = "White"
+    val colorCode = "#FFFFFF"
+    val phoneModel = "iPhone 15"
+    val screenSize = 6.1
+    val weight = 200.6f
+    val nationalDay = "Republic Day of Turkiye"
+    val holiday = "2024-08-30"
+    val reservationDate = "2024-07-07"
+    val streetName = "Istiklal Avenue"
+    val busLine = "41AT"
+    val remainingMinutes = 5
+    val trackingCode = "123456789"
+    val couponDuration = "2024-08-31"
+    val couponCode = "DISCOUNT10"
+    val billingAddress = "Uskudar, Istanbul"
+    
+}
+
