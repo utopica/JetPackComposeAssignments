d04fe0dd26e5357588330167f38f0bddeac4de4e
Elif OKUMUŞ <98635931+utopica@users.noreply.github.com>
2024-09-03 00:13:25 +0300

    fix column scrollable

--- AppUIDesign/app/src/main/java/com/example/appdesignassignment/uix/views/HomePage.kt
+++ AppUIDesign/app/src/main/java/com/example/appdesignassignment/uix/views/HomePage.kt
@@ -16,16 +16,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
@@ -42,11 +47,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -59,6 +66,7 @@ import com.example.appdesignassignment.data.entity.Pins
 import com.example.appdesignassignment.ui.theme.DarkModeIconColor
 import com.example.appdesignassignment.ui.theme.IconColor
 import com.example.appdesignassignment.ui.theme.arialBold
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -101,7 +109,6 @@ fun HomePage(navController: NavController, darkTheme: Boolean = isSystemInDarkTh
         bottomBar = {
             NavigationBar(
                 containerColor = backgroundColor,
-                modifier = Modifier.padding(0.dp)
             ) {
                 val navigationItems = listOf(
                     Icons.Default.Home to "Home",
@@ -153,17 +160,22 @@ fun HomePage(navController: NavController, darkTheme: Boolean = isSystemInDarkTh
                     containerColor = backgroundColor,
                     contentColor = textColor,
                     edgePadding = 16.dp,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(backgroundColor),
                     indicator = { tabPositions ->
-                        TabRowDefaults.Indicator(
-                            Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex.value])
-                                .widthIn(min = 10.dp)
-                                .padding(horizontal = 8.dp),
+                        TabRowDefaults. SecondaryIndicator(
+                            Modifier
+                                .tabIndicatorOffset(tabPositions[selectedTabIndex.value])
+                                .width(tabPositions[selectedTabIndex.value].contentWidth)
+                                .clip(RoundedCornerShape(5.dp)),
+                            height = 5.dp,
                             color = textColor
                         )
-                    }
+
+                    },
+                    divider = { // Remove the default gray divider line
+                        Divider(color = Color.Transparent)
+                    },
+
+
                 ) {
                     boardList.forEachIndexed { index, board ->
                         Tab(
@@ -182,33 +194,35 @@ fun HomePage(navController: NavController, darkTheme: Boolean = isSystemInDarkTh
                 }
             }
 
-            Spacer(modifier = Modifier.height(8.dp)) // Add spacing between tabs and content
+            Spacer(modifier = Modifier.height(8.dp))
 
+            val scrollState = rememberScrollState()
 
-            // Content Grid
             Row(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(2.dp)
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
             ) {
-                LazyColumn(
-                    verticalArrangement = Arrangement.spacedBy(5.dp),
+
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .wrapContentHeight()
+                        .weight(1f)
+                        .padding(start = 8.dp, end = 4.dp)
                 ) {
-                    items(pinList.filterIndexed { index, _ -> index % 2 == 0 }) { pin ->
+                    pinList.filterIndexed { index, _ -> index % 2 == 0 }.forEach { pin ->
                         PinItem(pin)
+                        Spacer(modifier = Modifier.height(8.dp))
                     }
                 }
-                LazyColumn(
-                    verticalArrangement = Arrangement.spacedBy(5.dp),
+
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight()
+                        .weight(1f)
+                        .padding(start = 4.dp, end = 8.dp)
                 ) {
-                    items(pinList.filterIndexed { index, _ -> index % 2 == 1 }) { pin ->
+                    pinList.filterIndexed { index, _ -> index % 2 == 1 }.forEach { pin ->
                         PinItem(pin)
+                        Spacer(modifier = Modifier.height(8.dp))
                     }
                 }
             }
@@ -217,54 +231,42 @@ fun HomePage(navController: NavController, darkTheme: Boolean = isSystemInDarkTh
 }
 
 @Composable
-fun PinItem(pin: Pins) {
-    val activity = (LocalContext.current as Activity)
-
+fun PinItem(pin: Pins, darkTheme: Boolean = isSystemInDarkTheme()) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(5.dp),
-        shape = RoundedCornerShape(20.dp)
+            .padding(4.dp),
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Box(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(20.dp))
-            ) {
-                Image(
-                    bitmap = ImageBitmap.imageResource(
-                        id = activity.resources.getIdentifier(pin.pinUrl, "drawable", activity.packageName)
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
+        Column {
+            Image(
+                bitmap = ImageBitmap.imageResource(
+                    id = LocalContext.current.resources.getIdentifier(pin.pinUrl, "drawable", LocalContext.current.packageName)
+                ),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth().background(if (darkTheme) Black else White).clip(RoundedCornerShape(20.dp))
+            )
 
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color(0xFFEFEFEF))
+                    .background(if (darkTheme) Black else White)
                     .padding(4.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
             ) {
                 Image(
                     bitmap = ImageBitmap.imageResource(
-                        id = activity.resources.getIdentifier(pin.pinProfilePictureUrl, "drawable", activity.packageName)
+                        id = LocalContext.current.resources.getIdentifier(pin.pinProfilePictureUrl, "drawable", LocalContext.current.packageName)
                     ),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(40.dp)
+                        .size(38.dp)
                         .clip(CircleShape)
                 )
 
                 Image(
-                    modifier = Modifier
-                        .clickable { /* Handle click */ }
-                        .size(28.dp),
                     painter = painterResource(id = R.drawable.threedotblack),
-                    contentDescription = null,
+                    contentDescription = "More options",
+                    modifier = Modifier.size(28.dp),
                 )
             }
         }
