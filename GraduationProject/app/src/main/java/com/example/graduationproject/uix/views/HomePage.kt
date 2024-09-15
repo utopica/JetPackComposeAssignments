492c54bc517976f500c63128311ba75bbc15ebd8
Elif OKUMUŞ <98635931+utopica@users.noreply.github.com>
2024-09-15 13:27:51 +0300

    added icon change

--- GraduationProject/app/src/main/java/com/example/graduationproject/uix/views/HomePage.kt
+++ GraduationProject/app/src/main/java/com/example/graduationproject/uix/views/HomePage.kt
@@ -1,14 +1,16 @@
 package com.example.graduationproject.uix.views
 
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,12 +18,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -31,10 +30,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -54,11 +51,9 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.graduationproject.R
 import com.example.graduationproject.data.entities.Foods
-import com.example.graduationproject.ui.theme.Peach
 import com.example.graduationproject.ui.theme.PeachText
 import com.example.graduationproject.ui.theme.poppinsMedium
 import com.example.graduationproject.uix.viewmodel.HomePageViewModel
@@ -85,17 +80,19 @@ fun HomePage(navController: NavController, homePageViewModel: HomePageViewModel)
                     SearchBar(
                         searchQuery = searchQuery,
                         onSearchQueryChange = { searchQuery = it },
-                        modifier = Modifier.fillMaxWidth(0.9f)
+                        modifier = Modifier
+                            .fillMaxWidth(0.9f)
+                            .height(40.dp)
                     )
                 },
                 navigationIcon = {
                     IconButton(onClick = {  }) {
-                        Icon(painterResource(id = R.drawable.menu), contentDescription = "Menu")
+                        Icon(painterResource(id = R.drawable.menu), modifier = Modifier.size(24.dp), contentDescription = "Menu")
                     }
                 },
                 actions = {
                     IconButton(onClick = {  }) {
-                        Icon(painterResource(id = R.drawable.filter), contentDescription = "Filter")
+                        Icon(painterResource(id = R.drawable.filter), modifier = Modifier.size(24.dp), contentDescription = "Filter")
                     }
                 }
             )
@@ -191,7 +188,15 @@ fun RecommendedSection(foodsList: List<Foods>, homePageViewModel: HomePageViewMo
 @Composable
 fun FoodItem(food: Foods, homePageViewModel: HomePageViewModel) {
 
-    var isAnimating by remember { mutableStateOf(false) }
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.cart)
+    )
+    var isPlaying by remember { mutableStateOf(false) }
+    var progress by remember { mutableStateOf(0f) }
+    val cartSize by animateDpAsState(
+        targetValue = 300.dp,
+        animationSpec = tween(durationMillis = 500)
+    )
 
     LaunchedEffect(food.food_picName) {
         homePageViewModel.getFoodImage(food.food_picName)
@@ -215,13 +220,13 @@ fun FoodItem(food: Foods, homePageViewModel: HomePageViewModel) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .aspectRatio(1f),
-                contentScale = ContentScale.Crop,
-                placeHolder = painterResource(R.drawable.placeholder),
+                contentScale = ContentScale.Crop
+
             )
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
+                horizontalArrangement = Arrangement.SpaceAround,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Column {
@@ -243,18 +248,30 @@ fun FoodItem(food: Foods, homePageViewModel: HomePageViewModel) {
 
                 Box(
                     modifier = Modifier
-                        .size(72.dp)
-                        .padding(4.dp)
-                        .clickable { isAnimating = !isAnimating }
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.CenterEnd
                 ) {
-                    val composition by rememberLottieComposition(
-                        spec = LottieCompositionSpec.RawRes(R.raw.cart)
-                    )
                     LottieAnimation(
                         composition = composition,
-                        iterations = if (isAnimating) LottieConstants.IterateForever else 1,
-                        modifier = Modifier.fillMaxSize()
+                        modifier = Modifier.size(70.dp).clickable {
+                            isPlaying = true
+                            progress = 0f
+                        },
+                        progress = { progress },
                     )
+
+                    LaunchedEffect(isPlaying) {
+                        if (isPlaying) {
+                            animate(
+                                0f,
+                                1f,
+                                animationSpec = tween(durationMillis = 1000)
+                            ) { value, _ ->
+                                progress = value
+                            }
+                            isPlaying = false
+                        }
+                    }
                 }
             }
 
